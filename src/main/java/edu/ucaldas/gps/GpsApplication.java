57090d52a3e6327068ad9c856258e503d55e1d9c
Seveco22 <sergio.velasquez35946@ucaldas.edu.co>
2024-04-09 19:48:06 -0500

    Geiner

--- src/main/java/edu/ucaldas/gps/GpsApplication.java
+++ src/main/java/edu/ucaldas/gps/GpsApplication.java
@@ -13,6 +13,6 @@ public class GpsApplication {
     }
     @GetMapping("/hello")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-      return String.format("Hello %s!", name);
+      return String.format("hola %s!", name);
     }
 }
